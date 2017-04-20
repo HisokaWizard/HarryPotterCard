93d0bb1d16e4be13d3ada4e022b97a18c47f4c8f
hisoka-wizard <npa040493@mail.ru>
2017-04-20 18:50:21 +0300

    view in exam pages

--- ForMyBride/controller/exam-sov.js
+++ ForMyBride/controller/exam-sov.js
@@ -3,16 +3,20 @@
 app.controller("ExamSOVCtrl", function($http, $location, sumTrueAnswer){
     var _public = this;
 
+    _public.password = "TonyaIsTheBestGirlInTheWord1998";
     _public.question_id = "0";
 
-    $http.get("http://localhost:3055/students")
-        .success(function(result){
-            console.log("Get Success students", result);
-            _public.Student = result;
-        })
-        .error(function(result){
-            console.log("Get Error students", result);
-        });
+    _public.GetStudents = function() {
+        $http.get("http://localhost:3055/students")
+            .success(function (result) {
+                console.log("Get Success students", result);
+                _public.Student = result;
+            })
+            .error(function (result) {
+                console.log("Get Error students", result);
+            });
+    };
+    _public.GetStudents();
 
     _public.addStudent = function(student){
         if(undefined == student){
@@ -26,6 +30,37 @@ app.controller("ExamSOVCtrl", function($http, $location, sumTrueAnswer){
             .error(function(result){
                console.log("Error in student post", result);
             });
+    };
+
+    _public.DeleteStudents = function(){
+      $http.delete("http://localhost:3055/students")
+          .success(function(result){
+              console.log("Success in student delete", result);
+          })
+          .error(function(result){
+              console.log("Error in student delete", result);
+          });
+    };
+
+    _public.flagDeleteResult = false;
+    _public.ToDeleteResult = function(){
+        if(_public.flagDeleteResult){
+            _public.flagDeleteResult = false;
+        }
+        else{
+            _public.flagDeleteResult = true;
+        }
+    };
+
+    _public.CheckPassword = function(password){
+        if(_public.password == password){
+            _public.flagDeleteResult = false;
+            _public.DeleteStudents();
+            _public.GetStudents();
+        }
+        else{
+            _public.flagDeleteResult = false;
+        }
     };
 
     _public.GetResult = function(){
